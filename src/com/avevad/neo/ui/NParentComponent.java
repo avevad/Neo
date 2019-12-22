1c2db8e3fe328f799d7cdb53f8e4b2e9da90314e
avevad <avevad0808@gmail.com>
2019-12-22 13:31:50 +0500

    implemented some of NComponent methods in NParentComponent

--- src/com/avevad/neo/ui/NParentComponent.java
+++ src/com/avevad/neo/ui/NParentComponent.java
@@ -43,4 +43,33 @@ public abstract class NParentComponent extends NComponent implements Iterable<NC
     public final Iterator<NComponent> iterator() {
         return children.iterator();
     }
+
+    @Override
+    public void onMousePressed(int x, int y, int button) {
+        for (NComponent comp : this) {
+            comp.onMousePressed(x - comp.getLocationX(), y - comp.getLocationY(), button);
+        }
+    }
+
+    @Override
+    public void onMouseReleased(int x, int y, int button) {
+        for (NComponent comp : this) {
+            comp.onMouseReleased(x - comp.getLocationX(), y - comp.getLocationY(), button);
+        }
+    }
+
+    @Override
+    public void onMouseDragged(int x, int y, int button) {
+        for (NComponent comp : this) {
+            comp.onMouseDragged(x - comp.getLocationX(), y - comp.getLocationY(), button);
+        }
+    }
+
+    @Override
+    public void onMouseMoved(int x, int y) {
+        for (NComponent comp : this) {
+            comp.onMouseMoved(x - comp.getLocationX(), y - comp.getLocationY());
+        }
+    }
+
 }
