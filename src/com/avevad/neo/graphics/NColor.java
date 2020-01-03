0ce66eca830d58a0e7595d483b436d4dcdfba36e
avevad <avevad0808@gmail.com>
2020-01-03 13:15:10 +0500

    added NColor class with some color constants

---
+++ src/com/avevad/neo/graphics/NColor.java
@@ -0,0 +1,15 @@
+package com.avevad.neo.graphics;
+
+public final class NColor {
+    private NColor() {
+    }
+
+    public static final int BLACK = 0x000000;
+    public static final int BLUE = 0x0000FF;
+    public static final int GREEN = 0x00FF00;
+    public static final int CYAN = 0x00FFFF;
+    public static final int RED = 0xFF0000;
+    public static final int MAGENTA = 0xFF00FF;
+    public static final int YELLOW = 0xFFFF00;
+    public static final int WHITE = 0xFFFFFF;
+}
