0d863de17d606cb93a9e04dea55759e91cf50f57
avevad <avevad0808@gmail.com>
2020-01-03 13:09:49 +0500

    overrode render method in NPanel

--- src/com/avevad/neo/ui/components/parent/NPanel.java
+++ src/com/avevad/neo/ui/components/parent/NPanel.java
@@ -1,5 +1,6 @@
 package com.avevad.neo.ui.components.parent;
 
+import com.avevad.neo.graphics.NGraphics;
 import com.avevad.neo.graphics.NRectangle;
 import com.avevad.neo.ui.NParentComponent;
 
@@ -10,6 +11,16 @@ public class NPanel extends NParentComponent {
 
     public NPanel(int x, int y, int w, int h) {
         super(x, y, w, h);
+    }
+
+    @Override
+    public boolean render(int layer) {
+        NGraphics g = getParent().getGraphics();
+        if (layer == 0) {
+            g.setColor(getBackgroundColor());
+            g.fillRect(getLocationX(), getLocationY(), getWidth(), getHeight());
+        }
+        return super.render(layer);
     }
 
     @Override
