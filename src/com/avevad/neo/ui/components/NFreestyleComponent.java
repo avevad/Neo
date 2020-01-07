100235c7ac80086f13c38f11346db8a70a9a41f7
avevad <avevad0808@gmail.com>
2020-01-07 13:33:01 +0500

    implemented the rest of NFreestyleComponent methods

--- src/com/avevad/neo/ui/components/NFreestyleComponent.java
+++ src/com/avevad/neo/ui/components/NFreestyleComponent.java
@@ -4,9 +4,21 @@ import com.avevad.neo.graphics.NGraphics;
 import com.avevad.neo.graphics.NImage;
 import com.avevad.neo.graphics.NRectangle;
 import com.avevad.neo.ui.NComponent;
+import com.avevad.neo.ui.NEventDispatcher;
+import com.avevad.neo.ui.events.*;
 
 public class NFreestyleComponent extends NComponent {
     public final NImage canvas;
+    private boolean keyboardNeeded = false;
+    public int zIndex = 0;
+
+    public final NEventDispatcher<NMousePressedEvent> mousePressed = new NEventDispatcher<>();
+    public final NEventDispatcher<NMouseReleasedEvent> mouseReleased = new NEventDispatcher<>();
+    public final NEventDispatcher<NMouseDraggedEvent> mouseDragged = new NEventDispatcher<>();
+    public final NEventDispatcher<NMouseWheelScrolledEvent> mouseWheelScrolled = new NEventDispatcher<>();
+    public final NEventDispatcher<NMouseMovedEvent> mouseMoved = new NEventDispatcher<>();
+    public final NEventDispatcher<NKeyPressedEvent> keyPressed = new NEventDispatcher<>();
+    public final NEventDispatcher<NKeyReleasedEvent> keyReleased = new NEventDispatcher<>();
 
     public NFreestyleComponent(NImage canvas, int x, int y, int w, int h) {
         super(x, y, w, h);
@@ -22,46 +34,65 @@ public class NFreestyleComponent extends NComponent {
 
     @Override
     public boolean onMousePressed(int x, int y, int button) {
-        return false;
+        if (x < 0 || x >= getWidth()) return false;
+        if (y < 0 || y >= getHeight()) return false;
+        mousePressed.trigger(new NMousePressedEvent(x, y, button));
+        return true;
     }
 
     @Override
     public boolean onMouseReleased(int x, int y, int button) {
-        return false;
+        if (x < 0 || x >= getWidth()) return false;
+        if (y < 0 || y >= getHeight()) return false;
+        mouseReleased.trigger(new NMouseReleasedEvent(x, y, button));
+        return true;
     }
 
     @Override
     public boolean onMouseDragged(int x, int y, int button) {
-        return false;
+        if (x < 0 || x >= getWidth()) return false;
+        if (y < 0 || y >= getHeight()) return false;
+        mouseDragged.trigger(new NMouseDraggedEvent(x, y, button));
+        return true;
     }
 
     @Override
     public boolean onMouseWheelScrolled(int x, int y, int value) {
-        return false;
+        if (x < 0 || x >= getWidth()) return false;
+        if (y < 0 || y >= getHeight()) return false;
+        mouseWheelScrolled.trigger(new NMouseWheelScrolledEvent(x, y, value));
+        return true;
     }
 
     @Override
     public boolean onMouseMoved(int x, int y) {
-        return false;
+        if (x < 0 || x >= getWidth()) return false;
+        if (y < 0 || y >= getHeight()) return false;
+        mouseMoved.trigger(new NMouseMovedEvent(x, y));
+        return true;
     }
 
     @Override
     public void onKeyPressed(int key) {
-
+        keyPressed.trigger(new NKeyPressedEvent(key));
     }
 
     @Override
     public void onKeyReleased(int key) {
+        keyReleased.trigger(new NKeyReleasedEvent(key));
+    }
 
+    public void setKeyboardNeeded(boolean keyboardNeeded) {
+        this.keyboardNeeded = keyboardNeeded;
     }
 
     @Override
     public boolean isKeyboardNeeded() {
-        return false;
+        return keyboardNeeded;
     }
 
     @Override
     public int getZIndex() {
-        return 0;
+        return zIndex;
     }
 }
