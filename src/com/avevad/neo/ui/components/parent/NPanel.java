1b60c40167cdbdd517a0643a5ae9498fe1443d87
avevad <avevad0808@gmail.com>
2020-01-08 16:32:37 +0500

    fixed NPanel being "transparent" in handling mouse events

--- src/com/avevad/neo/ui/components/parent/NPanel.java
+++ src/com/avevad/neo/ui/components/parent/NPanel.java
@@ -1,6 +1,7 @@
 package com.avevad.neo.ui.components.parent;
 
 import com.avevad.neo.graphics.NGraphics;
+import com.avevad.neo.graphics.NPoint;
 import com.avevad.neo.graphics.NRectangle;
 import com.avevad.neo.ui.NParentComponent;
 
@@ -21,5 +22,35 @@ public class NPanel extends NParentComponent {
             g.fillRect(getLocationX(), getLocationY(), getWidth(), getHeight());
         }
         return super.render(layer);
+    }
+
+    @Override
+    public boolean onMousePressed(int x, int y, int button) {
+        if (super.onMousePressed(x, y, button)) return true;
+        return new NRectangle(NPoint.ZERO, getSize()).contains(x, y);
+    }
+
+    @Override
+    public boolean onMouseReleased(int x, int y, int button) {
+        if (super.onMouseReleased(x, y, button)) return true;
+        return new NRectangle(NPoint.ZERO, getSize()).contains(x, y);
+    }
+
+    @Override
+    public boolean onMouseDragged(int x, int y, int button) {
+        if (super.onMouseDragged(x, y, button)) return true;
+        return new NRectangle(NPoint.ZERO, getSize()).contains(x, y);
+    }
+
+    @Override
+    public boolean onMouseMoved(int x, int y) {
+        if (super.onMouseMoved(x, y)) return true;
+        return new NRectangle(NPoint.ZERO, getSize()).contains(x, y);
+    }
+
+    @Override
+    public boolean onMouseWheelScrolled(int x, int y, int value) {
+        if (super.onMouseWheelScrolled(x, y, value)) return true;
+        return new NRectangle(NPoint.ZERO, getSize()).contains(x, y);
     }
 }
