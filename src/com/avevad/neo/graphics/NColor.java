896e3115dfe2d8df0a9bac54861c09a18371687f
avevad <avevad0808@gmail.com>
2020-01-10 20:52:57 +0500

    added some auxiliary methods to NColor.java

--- src/com/avevad/neo/graphics/NColor.java
+++ src/com/avevad/neo/graphics/NColor.java
@@ -14,4 +14,36 @@ public final class NColor {
     public static final int MAGENTA = 0xFF00FF;
     public static final int YELLOW = 0xFFFF00;
     public static final int WHITE = 0xFFFFFF;
+
+    public static int red(int color) {
+        return (color & RED) >> 16;
+    }
+
+    public static int green(int color) {
+        return (color & GREEN) >> 8;
+    }
+
+    public static int blue(int color) {
+        return color & BLUE;
+    }
+
+    public static int rgb(int r, int g, int b) {
+        return (r << 16) + (g << 8) + b;
+    }
+
+
+    public static int mix(int first, int second, double firstPart, double secondPart) {
+        int r = (int) (red(first) * firstPart + red(second) * secondPart);
+        int g = (int) (green(first) * firstPart + green(second) * secondPart);
+        int b = (int) (blue(first) * firstPart + blue(second) * secondPart);
+        return rgb(r, g, b);
+    }
+
+    public static int mix(int first, int second, double firstPart) {
+        return mix(first, second, firstPart, 1 - firstPart);
+    }
+
+    public static int mix(int first, int second) {
+        return mix(first, second, 0.5, 0.5);
+    }
 }
