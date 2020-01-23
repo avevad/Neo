dc1d8f5e72f7d261e5b178f6cab4a7a3c196f3da
avevad <avevad0808@gmail.com>
2020-01-23 18:50:19 +0500

    added varargs to NMultiLogDestination constructor

--- src/com/avevad/neo/logging/NMultiLogDestination.java
+++ src/com/avevad/neo/logging/NMultiLogDestination.java
@@ -1,10 +1,15 @@
 package com.avevad.neo.logging;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class NMultiLogDestination implements NLogDestination {
     public Set<NLogDestination> destinations = new HashSet<>();
+
+    public NMultiLogDestination(NLogDestination... destinations) {
+        this.destinations.addAll(Arrays.asList(destinations));
+    }
 
     @Override
     public void write(NLogMessage message) {
