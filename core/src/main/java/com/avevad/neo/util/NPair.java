62ea082c16cc2420eb32116cb8b3a92acb67f856
avevad <avevad0808@gmail.com>
2020-02-15 21:16:14 +0500

    added pair util class

---
+++ core/src/main/java/com/avevad/neo/util/NPair.java
@@ -0,0 +1,11 @@
+package com.avevad.neo.util;
+
+public class NPair<A, B> {
+    public final A a;
+    public final B b;
+
+    public NPair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+}
